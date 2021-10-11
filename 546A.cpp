d4ca2db7d82722533e257d46dd1b6fb699604df8
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-10-11 21:51:24 +0600

    mahirlabibdihan

--- 546A.cpp
+++ 546A.cpp
@@ -1,10 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-  long long k,s,n; cin>>k>>s>>n; n=(n*(n+1)*k)/2;
+  int k,n,w;  // 0<=n<=10^9  , 1<=k,w<=1000
+  cin>>k>>n>>w; 
+  int x=(w*(w+1)*k)/2;  
 
-  if(s>n) cout<<"0"; else printf("%lld",n-s);
+  /* 
+  1.k + 2.k + ..... + i.k + .... + w.k 
+  => k(1+2+.....+i+....+w) 
+  => k*(w*(w+1)/2) 
+  => Total cost of buying w bananas
+  if this is greater than n, than soldier need to borrow .
+  */
+
+  if(n>=x) cout<<0; 
+  else cout<<x-n;
 }
 
