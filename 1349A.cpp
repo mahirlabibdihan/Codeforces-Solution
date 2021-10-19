b0b27fa4f351fe682c427b55d940c58e741d7d06
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2021-10-19 22:06:21 +0600

    Update 1349A.cpp

--- 1349A.cpp
+++ 1349A.cpp
@@ -1,33 +1,58 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-
-int gcd(int a,int b)
-
+/*
+  By analyzing,I have found out that,
+  gcd({lcm({x[i],x[j]})|i<j}) 
+  is equivalent to
+  gcd({ lcm(x[i], gcd({x[j]|i<j}) ) })
+  
+  So, for every i,
+  S-1:first I need to calculate gcd of all x[j] where j>i.
+  S-2:Then I will calculate lcm of x[i] and gcd of S-1.
+  S-3:Lastly I will calculate gcd of all lcm of S-2.
+*/
+long long gcd(long long a,long long b)
 {
     while(a)
-     {
-        int c=a;
-        a=b%a;
-        b=c;
-     }
-
-     return b;
+    {
+       long long c=a;
+       a=b%a;
+       b=c;
+    }
+    return b;
 }
 
+long long lcm(long long a,long long b)
+{
+    return a*b/gcd(a,b);
+}
 
 int main()
 {
+    int n; 
+    cin>>n; 
+    int x[100000];
+    for(int i=0;i<n;i++)
+    {
+      cin>>x[i];
+    }
 
-    int n,i,a=0,b,LCM,GCD,j,k; cin>>n; int x[n],y[n*(n-1)/2];
-
-    for(i=0;i<n;i++) cin>>x[i];
-
-    a=gcd(x[n-1],x[n-2]);
-    b=x[0]*x[1]/gcd(x[0],x[1]);
-    GCD=gcd(a,b);
-
-
-    cout<<GCD;
+    long long ans=0,lastGcd,Gcd,Lcm;
+    for(int i=n-2;i>=0;i--)
+    {
+      // S-1
+      if(i==n-2) Gcd=x[i+1];
+      else Gcd=gcd(x[i+1],lastGcd); 
+      
+      // S-2
+      Lcm = lcm(x[i],Gcd);
+      
+      // S-3
+      if(ans==0) ans=Lcm;
+      else ans=gcd(ans,Lcm);
+      
+      lastGcd=Gcd;
+    }
+    cout<<ans;
 }
-
