bac73c957e67280f73fe41e001b6f4bdfd9a4f06
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-12-14 01:44:26 +0600

    dihu

---
+++ Template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+    
+}
+int main()
+{
+    int t;
+    while(t--)
+    {
+        solve();
+    }
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+    }
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        int type, l, r;
+        cin >> type >> l >> r;
